94c9c8c797de3d5ff6ea8641ecb47b9be8acc654
ruen <ruen.zapanta@gmail.com>
2018-08-24 08:51:42 +0800

    removed h2 console

--- src/main/java/com/khakiout/study/ddddemo/DddDemoApplication.java
+++ src/main/java/com/khakiout/study/ddddemo/DddDemoApplication.java
@@ -2,12 +2,9 @@ package com.khakiout.study.ddddemo;
 
 import com.khakiout.study.ddddemo.domain.entity.User;
 import com.khakiout.study.ddddemo.infrastructure.UserRepository;
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DddDemoApplication {
@@ -20,12 +17,5 @@ public class DddDemoApplication {
 		user.setFirstName("Larry");
 		user.setLastName("Bob");
 		userRepository.create(user);
-	}
-
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-		registration.addUrlMappings("/console/*");
-		return registration;
 	}
 }

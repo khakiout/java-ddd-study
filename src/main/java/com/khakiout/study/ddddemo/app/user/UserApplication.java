70757cfcf330755fe5c65bbed6074af1165680ff
ruen <ruen.zapanta@gmail.com>
2018-09-03 13:19:05 +0800

    #4 - add reactive todo

--- src/main/java/com/khakiout/study/ddddemo/app/user/UserApplication.java
+++ src/main/java/com/khakiout/study/ddddemo/app/user/UserApplication.java
@@ -10,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * TODO: add reactive
+ */
 @Service
 public class UserApplication implements BaseApplication<UserDTO> {
 

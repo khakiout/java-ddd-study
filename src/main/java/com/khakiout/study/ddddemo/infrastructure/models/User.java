0029cf4e068a9f495f74cdc2d5fddc46b7399436
ruen <ruen.zapanta@gmail.com>
2018-09-14 23:23:46 +0800

    moved common fields to base model

--- src/main/java/com/khakiout/study/ddddemo/infrastructure/models/User.java
+++ src/main/java/com/khakiout/study/ddddemo/infrastructure/models/User.java
@@ -1,21 +1,12 @@
 package com.khakiout.study.ddddemo.infrastructure.models;
 
-import java.util.Date;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
 @Entity
 public class User extends BaseModel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
-    private Long id;
     @NotNull
     private String firstName;
     @NotNull
@@ -25,26 +16,8 @@ public class User extends BaseModel {
     @Email(regexp = ".+@.+\\..+")
     private String email;
 
-    @Column(nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    private Date updatedAt;
-
     public User() {
 
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
@@ -71,19 +44,4 @@ public class User extends BaseModel {
         this.email = email;
     }
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
